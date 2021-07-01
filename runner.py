084952f1b60f012bf8ebd99177b9eb98a81b6044
Rowan_Surface <55083730+rej696@users.noreply.github.com>
2021-07-01 15:44:07 +0100

    add show_log and interactive commands

--- runner.py
+++ runner.py
@@ -1,6 +1,10 @@
 import os
+import sys
+import pty
 import subprocess
 import argparse
+
+IMAGE = "rej696/pico-ada-builder:latest"
 
 if __name__ == "__main__":
 
@@ -25,25 +29,72 @@ if __name__ == "__main__":
         help="run gnatprove on specified files, defaults to main.adb"
     )
 
+    parser.add_argument(
+        "--build_image",
+        action="store_true",
+        help="build the pico-ada-builder docker image from the dockerfile"
+    )
+
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="Run a docker interactive shell for the pico-ada-builder image"
+    )
+
+    parser.add_argument(
+        "-l",
+        "--print_log",
+        action="store_true",
+        help="Print the log to the stdout after running a build/prove command"
+    )
+
     args = parser.parse_args()
     args.prove = True if args.prove == [] else args.prove
+
+    if (not args.build
+            and not args.build_image
+            and not args.prove
+            and not args.interactive
+            and not args.print_log):
+        print("No arguments given, run python runner.py --help for options")
+        sys.exit()
 
     docker_cmd = [
         "docker",
         "run",
         "--rm",
-        "-it",
         "-v",
         f"{os.getcwd()}:/build",
-        "rej696/pico-ada-builder:latest"
+        IMAGE
     ]
+
+    if args.build_image:
+        print("Building pico-ada-builder Docker Image")
+        subprocess.run(
+            ["docker",
+             "build",
+             "-t",
+             IMAGE,
+             "docker/pico-ada-builder/"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE
+        )
+        print("pico-ada-builder Image Created")
 
     if args.build:
         print("Building firmware")
-        subprocess.run(docker_cmd, capture_output=True, check=True)
+        subprocess.run(
+            docker_cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        print("Build complete")
 
     if args.prove:
-        print(f"Running gnatprove on {args.prove}")
         docker_cmd.extend([
             "bash",
             "/build/docker/pico-ada-builder/prove.sh"
@@ -51,8 +102,44 @@ if __name__ == "__main__":
 
         if isinstance(args.prove, list):
             docker_cmd.extend(args.prove)
-        
-        subprocess.run(docker_cmd, capture_output=True, check=True)
+            print(f"Running gnatprove on {args.prove}")
+        else:
+            print("Running gnatprove on main.adb")
 
-    if not args.prove and not args.build:
-        print("No arguments given, run python runner.py --help for options")
+        subprocess.run(
+            docker_cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        print("gnatprove complete, output in logs/prove.log")
+
+    if args.print_log:
+        if args.prove:
+            with open("logs/prove.log", "r") as f:
+                print(f.read())
+        else:
+            with open("logs/build.log", "r") as f:
+                print(f.read())
+
+    if args.interactive:
+        print("Opening interactive container")
+#         pty, tty = pty.openpty()
+#         subprocess.run(
+#             ["docker",
+#              "run",
+#              "-it",
+#              "--rm",
+#              "-v",
+#              f"{os.getcwd()}:/build",
+#              IMAGE,
+#              "bash"],
+#             check=True,
+#             stdout=subprocess.PIPE,
+#             stdin=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             shell=True
+#         )
+        os.system(f"docker run -it --rm -v {os.getcwd()}:/build {IMAGE} bash")
+        print("Interactive container closed")
